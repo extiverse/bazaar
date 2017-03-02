819c3a00dc58c24c7cb03ddc2564a2cc562bc396
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-03-02 23:02:43 +0100

    Finish update command tests

--- tests/Bazaar/Composer/UpdateCommandTest.php
+++ tests/Bazaar/Composer/UpdateCommandTest.php
@@ -10,7 +10,8 @@ class UpdateCommandTest extends BaseCommandTest
             'name' => 'flagrow/test',
             'type' => 'project',
             'require' => [
-                'nesbot/carbon' => '1.22.0',
+                'nesbot/carbon' => '1.22.0', // An update to at least 1.22.1 is available
+                'symfony/yaml' => 'v3.2.3', // An update to at least 3.2.4 is available
             ],
         ]);
 
@@ -23,6 +24,7 @@ class UpdateCommandTest extends BaseCommandTest
             'type' => 'project',
             'require' => [
                 'nesbot/carbon' => '^1.22.0',
+                'symfony/yaml' => '^v3.2.3',
             ],
         ]);
 
@@ -30,5 +32,42 @@ class UpdateCommandTest extends BaseCommandTest
         $this->getComposerCommand()->update('nesbot/carbon');
         $this->assertPackageInstalled('nesbot/carbon');
         $this->assertPackageVersionNotInstalled('nesbot/carbon', '1.22.0'); // Check if old version is no longer used
+
+        // Check other package was not updated
+        $this->assertPackageInstalled('symfony/yaml', 'v3.2.3');
+    }
+
+    public function testUpdateAllPackages()
+    {
+        $this->setComposerJson([
+            'name' => 'flagrow/test',
+            'type' => 'project',
+            'require' => [
+                'nesbot/carbon' => '1.22.0', // An update to at least 1.22.1 is available
+                'symfony/yaml' => 'v3.2.3', // An update to at least 3.2.4 is available
+            ],
+        ]);
+
+        // Install an old version on purpose
+        $this->getComposerCommand()->update();
+        $this->assertPackageInstalled('nesbot/carbon', '1.22.0');
+        $this->assertPackageInstalled('symfony/yaml', 'v3.2.3');
+
+        $this->setComposerJson([
+            'name' => 'flagrow/test',
+            'type' => 'project',
+            'require' => [
+                'nesbot/carbon' => '^1.22.0',
+                'symfony/yaml' => '^v3.2.3',
+            ],
+        ]);
+
+        // Update all
+        $this->getComposerCommand()->update();
+        $this->assertPackageInstalled('nesbot/carbon');
+        $this->assertPackageVersionNotInstalled('nesbot/carbon', '1.22.0'); // Check if old version is no longer used
+        $this->assertPackageInstalled('symfony/yaml');
+        $this->assertPackageVersionNotInstalled('symfony/yaml', 'v3.2.3');
+
     }
 }
