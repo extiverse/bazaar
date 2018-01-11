7f890670c6fbb240d5a62918d9c88cfd4273efb2
Daniel Klabbers <daniel@productip.com>
2018-01-11 13:26:08 +0100

    added logos and moving files to resources

--- src/Listeners/AddClientAssets.php
+++ src/Listeners/AddClientAssets.php
@@ -20,7 +20,7 @@ class AddClientAssets
         if ($event->isAdmin()) {
             $event->addAssets([
                 __DIR__.'/../../js/admin/dist/extension.js',
-                __DIR__.'/../../less/extension.less',
+                __DIR__.'/../../resources/less/extension.less',
             ]);
             $event->addBootstrapper('flagrow/bazaar/main');
         }
@@ -28,7 +28,7 @@ class AddClientAssets
 
     public function addLocales(ConfigureLocales $event)
     {
-        foreach (new DirectoryIterator(__DIR__.'/../../locale') as $file) {
+        foreach (new DirectoryIterator(__DIR__.'/../../resources/locale') as $file) {
             if ($file->isFile() && in_array($file->getExtension(), ['yml', 'yaml'])) {
                 $event->locales->addTranslations($file->getBasename('.'.$file->getExtension()), $file->getPathname());
             }
